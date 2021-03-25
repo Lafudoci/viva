8f978c9c64e303aa92686fe407cdcf63e10932e7
Lafudoci <chunhsi.tso@gmail.com>
2021-03-25 10:39:14 +0800

    Add rhesus_monkey genome

--- reads_preprocess.py
+++ reads_preprocess.py
@@ -110,6 +110,9 @@ def remove_host(task):
     elif task.dehost == 'chicken':
         dehost_meta['genome'] = 'Chicken (GRCg6a, GCF_000002315.6)'
         genome_path = Path(Path.home(), 'genome', 'grcg6a', 'grcg6a')
+    elif task.dehost == 'rhesus_monkey':
+        dehost_meta['genome'] = 'Rhesus monkey (Mmul_10, GCF_003339765.1)'
+        genome_path = Path(Path.home(), 'genome', 'mmul_10', 'mmul_10')
 
     unconc_reads_out = task.id + '_host_removed_R%.fastq.gz'
     mapped_reads_out = 'host_mapped.sam'
