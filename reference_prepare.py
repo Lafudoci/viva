d4e76ca16a4cb53ac3f3aa20dcfca2dcb413f0b3
Lafudoci <chunhsi.tso@gmail.com>
2021-03-26 10:49:15 +0800

    Suppress spades info when no denovo

--- reference_prepare.py
+++ reference_prepare.py
@@ -104,9 +104,12 @@ def ref_import(task):
             'file_name': file_name,
             'fasta_header': fasta_header,
             'user_provide': task.with_ref,
-            'spades_mode': task.spades_mode,
             'seq_length': str(len(first_fasta_seq))
         }
+        if task.with_ref == False:
+            meta_dict['spades_mode'] = task.spades_mode
+        else:
+            meta_dict['spades_mode'] = 'N/A'
         utils.build_json_file(imported_ref_meta_path, meta_dict)
     else:
         logger.error('Reference sequence file must be single squence.')
