19eb08685d77f37fe5966037ce59e49260917abd
lafudoci <chunhsi.tso@gmail.com>
2022-05-17 14:09:18 +0800

    Add unmapped BLAST filter

--- src/unmapped_analysis.py
+++ src/unmapped_analysis.py
@@ -37,14 +37,16 @@ def run_de_novo(task):
 def blast_assembled(task):
     logger.info('BLASTing unmapped reads assembled.')
     assembled_cwd = task.path.joinpath(task.id, 'assembly', 'unmapped', '%s_unmapped_spades_%s'%(task.id, task.unmapped_spades_mode))
+    qeury_filename = 'contigs.fasta'
+    blast_result_filename = '%s_spades_%s.tsv'%(task.id, task.unmapped_spades_mode)
     blast_cmd = [
         'blastn',
         '-db',
         task.unmapped_blastdb,
         '-query',
-        'contigs.fasta',
+        qeury_filename,
         '-out',
-        '%s_spades_%s.tsv'%(task.id, task.unmapped_spades_mode),
+        blast_result_filename,
         '-outfmt',
         '6 qseqid sacc pident qlen length evalue stitle',
         '-num_threads',
@@ -57,6 +59,36 @@ def blast_assembled(task):
     cmd_run = subprocess.run(blast_cmd, cwd=assembled_cwd, capture_output=True)
     print(cmd_run.stdout.decode(encoding='utf-8'))
     print(cmd_run.stderr.decode(encoding='utf-8'))
+    
+    # filter highly matched hits
+    logger.info('Filter highly matched hits')
+    blast_result_path = assembled_cwd.path.joinpath(blast_result_filename)
+    highly_match_result_list = []
+    with open(blast_result_path, 'r') as f:
+        for line in f.readlines():
+            hit = line.strip().split('\t')
+            # identity
+            if Decimal(hit[2]) <= Decimal('90'):
+                break
+            # mapped length
+            if Decimal(hit[4]) <= Decimal('200'):
+                break
+            # query coverage
+            if Decimal(hit[4])/Decimal(hit[3]) <= Decimal('0.8'):
+                break
+            hit_list = [
+                hit[0],
+                hit[1],
+                hit[2],
+                hit[3],
+                hit[4],
+                hit[5],
+                hit[6]
+            ]
+            highly_match_result_list.append(hit_list)
+    
+    highly_match_result_list_json_path = assembled_cwd.path.joinpath('highly_match_result_list.json')
+    utils.build_json_file(highly_match_result_list_json_path, {highly_match_result_list})
 
 
 def run(task):
